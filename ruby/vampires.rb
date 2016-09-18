fb56d3d8a92d1e807e01efd67ebe743a698823ab
Alex J. McLeod <mcleoda@yahoo.com>
2016-09-17 21:50:53 -0400

    Convert answers to last two questions to booleans, now testing conditional statement 1

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,42 @@
 puts "What is your name?"
 name=gets.chomp 
-puts "How old are you? What year were you born?"
-age=gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "How old are you?"
+age=gets.chomp.to_i
+puts "What year were you born?"
+year=gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
 food=gets.chomp
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (yes/no)"
 insurance=gets.chomp
+
+if food=="yes"
+  food=true
+elsif food=="no"
+  food=false
+else
+  food=nil
+end
+
+if insurance=="yes"
+  insurance=true
+elsif insurance=="no"
+  insurance=false
+else
+  insurance=nil
+end
+
+
+yearDiff=2016-age
+correctYear=(yearDiff==year)
+puts (correctYear && (food || insurance))
+# if (correctYear && (food || insurance))==true
+  # puts "Probably not a vampire"
+# elsif !((correctYear && food) || insurance)==false
+  # puts "Probably a vampire"
+# elsif !(correctYear && food && insurance)==false
+  # puts "Almost certainly a vampire"
+# elsif (name=="Drake Cula" || name=="Tu Fang")==true
+  # puts "Definitely a vampire"
+# else
+  # puts "Results inconclusive"
+# end
