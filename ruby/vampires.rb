3f35410da4b791abdf07c6cb62a96e919db69904
Alex J. McLeod <mcleoda@yahoo.com>
2016-09-17 21:30:25 -0400

    Finished code for release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+name=gets.chomp 
+puts "How old are you? What year were you born?"
+age=gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+food=gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance=gets.chomp
