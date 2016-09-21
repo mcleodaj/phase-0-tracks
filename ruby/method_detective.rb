ee00967e3506adfc5f88de823196a2e6ff8bc8a8
Alex J. McLeod <mcleoda@yahoo.com>
2016-09-21 14:50:37 -0400

    updated file with required info for 4.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,33 +7,33 @@
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".sub("o","oo")
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".center(20)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# "The case of the disappearing last letter".slice(0...-1)
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# "The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
