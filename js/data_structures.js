9e3e2631987209838f980d4131b297a40972f172
Alex J. McLeod <mcleoda@yahoo.com>
2016-10-05 10:18:33 -0400

    Updated with horses object

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,16 @@ names.push("Spirit")
 
 console.log(colors)
 console.log(names)
+
+var horses={}
+
+// Use a for loop, that increments, and for each incrementation
+// assign the color of that index to the horse of that index
+// within our hash
+
+for (var i=0; i<colors.length; i++)
+{
+  horses[names[i]]=colors[i]
+}
+
+console.log(horses)
