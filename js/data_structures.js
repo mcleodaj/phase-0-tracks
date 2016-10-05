934394824ccd4982173db65e66d0db2ad3388a67
Alex J. McLeod <mcleoda@yahoo.com>
2016-10-05 10:11:37 -0400

    Added initial arrays and push

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors=["Green", "Red", "Blue", "Yellow"]
+var names=["Silver", "Secretariat", "Seabiscuit", "Buck"]
+
+colors.push("Purple")
+names.push("Spirit")
+
+console.log(colors)
+console.log(names)
